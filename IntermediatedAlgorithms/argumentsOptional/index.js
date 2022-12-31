fe7db8ccda1ead2c2c302377471ac79e1cdbc1c6
andohfrancis9187gmail.com <andohfrancis9187@gmail.com>
2022-12-31 08:36:51 +0000

    adding up

---
+++ IntermediatedAlgorithms/argumentsOptional/index.js
@@ -0,0 +1,18 @@
+function addTogether() {
+    const [a,b] = arguments // destructuring argument object
+     const add = a + b
+    if (typeof(a) !== "number"){
+      return undefined
+    }
+     
+    if (arguments.length == 1){
+      return  (b) => addTogether(a,b)  // the order matters 
+    }  
+    if (typeof(b) !==  "number"){
+      return undefined
+    }
+    
+    return add;
+  }
+  
+  addTogether(2,3);
